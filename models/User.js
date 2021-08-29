74274d25160a0a723acf5996d1ae91047600288b
Cássia Martinelli <56940890+cmartinellicm@users.noreply.github.com>
2021-08-29 02:54:00 -0300

    Atualização de swagger

--- models/User.js
+++ models/User.js
@@ -17,7 +17,7 @@ const UserSchema = new mongoose.Schema(
 
     cep: { type: String, required: true },
     logradouro: { type: String, required: true },
-    numero: { type: String, required: true },
+    numero: { type: Number, required: true },
     bairro: { type: String, required: true },
     cidade: { type: String, required: true },
     estado: { type: String, required: true },
