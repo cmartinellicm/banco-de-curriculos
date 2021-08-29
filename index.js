8d1e4dc1a3c5f02e859d336aeeb5eb1d19d440c8
Cássia Martinelli <56940890+cmartinellicm@users.noreply.github.com>
2021-08-29 01:18:59 -0300

    Redefinicao de porta para o heroku

--- index.js
+++ index.js
@@ -4,49 +4,21 @@ const mongoose = require("mongoose");
 const swaggerUI = require("swagger-ui-express");
 const swaggerDocs = require("./swagger.json");
 const routes = require("./routes");
-const axios = require("axios");
 const app = express();
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 app.use(routes);
 
 // Definicao de porta
-const port = 3000;
+const PORT = process.env.PORT || 5000;
 
 // Conexão com banco de dados
 mongoose.connect("mongodb+srv://admin-cassia:gamadb@cluster0.uysjc.mongodb.net/test", { useNewUrlParser: true });
 
-// Buscar CEP via API externa - ViaCEP
-let cep = "51030340";
-axios
-  .get("https://viacep.com.br/ws/" + cep + "/json/")
-  .then(function (res) {
-    // console.log(res.data);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-
-// Servico de busca de usuarios cadastrados
-// app.get("/users", (req, res) => {
-//   connection.query("SELECT * FROM users_db.user", function (err, rows, fields) {
-//     if (err) throw err;
-
-//     res.send(rows);
-//   });
-// });
-
-// Servico de busca de usuarios por CPF
-// app.get("/users/:cpf", (req, res) => {
-//   connection.query("SELECT nome FROM users_db.user WHERE cpf = " + req.params.cpf, function (err, rows, fields) {
-//     if (err) throw err;
-
-//     res.send(rows);
-//   });
-// });
-
 // Subindo servidor Node
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT}`);
 });
